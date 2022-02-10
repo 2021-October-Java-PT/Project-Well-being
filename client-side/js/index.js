fc7559f3b502addce0a96ccafbc791d72ca3dd16
nichofer <nvasaris@kent.edu>
2022-02-10 16:24:38 -0500

    mood scale addition

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -5,6 +5,7 @@ import AllReminders from "./components/AllReminders";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Journal from "./components/Journal";
+import Mood from "./components/Mood";
 import Reminders from "./components/Reminders";
 import Resources from "./components/Resources";
 import apiHelpers from "./api-helpers.js/apiHelpers";
@@ -14,7 +15,7 @@ const app = document.querySelector("#app");
 
 buildPage();
 
-function buildPage(){
+function buildPage() {
     renderHome();
     navHome();
     navReminders();
@@ -24,61 +25,73 @@ function buildPage(){
     navResources();
 }
 
-function renderHome(){
+function renderHome() {
     app.innerHTML = Home();
     checkin();
 }
 
-function navHome(){
+function navHome() {
     const homeElem = document.querySelector(".nav-list__home");
     homeElem.addEventListener("click", () => {
-       app.innerHTML = Home();
-       checkin();
-    });   
+        app.innerHTML = Home();
+        checkin();
+    });
 }
 
 function checkin() {
-      const checkin = document.querySelector(".checkin");
-      checkin.addEventListener("click", () => {
-        app.innerHTML = Journal();
-     });   
- }
+    const checkin = document.querySelector(".checkin");
+    checkin.addEventListener("click", () => {
+        app.innerHTML = Mood();
+        var slider=document.getElementById("slider");
+        var emoji=document.getElementById("emoji");
+        var emoticons=["mood_bad",
+        "sentiment_very_dissatisfied",
+        "sentiment_satisfied",
+        "sentiment_satisfied_alt",
+        "sentiment_very_satisfied"
+        ];
 
-function navReminders(){
+        slider.oninput=function(){
+            emoji.innerHTML=emoticons[slider.value];
+            console.log(slider.value);
+        }
+    });
+}
+
+function navReminders() {
     const remindersElem = document.querySelector(".nav-list__reminders");
     remindersElem.addEventListener("click", () => {
         crud.getRequest("http://localhost:8080/api/reminders", reminders => {
             console.log(reminders);
             app.innerHTML = AllReminders(reminders);
-        }
-        );
-    });   
+        });
+    });
 }
 
-function navJournal(){
+function navJournal() {
     const journalElem = document.querySelector(".nav-list__journal");
     journalElem.addEventListener("click", () => {
-       app.innerHTML = Journal();
-    });   
+        app.innerHTML = Journal();
+    });
 }
 
-function navResources(){
+function navResources() {
     const journalElem = document.querySelector(".nav-list__resources");
     journalElem.addEventListener("click", () => {
-       app.innerHTML = Resources();
-    });   
+        app.innerHTML = Resources();
+    });
 }
 
-function navAbout(){
+function navAbout() {
     const aboutElem = document.querySelector(".nav-list__about");
     aboutElem.addEventListener("click", () => {
-       app.innerHTML = About();
-    });   
+        app.innerHTML = About();
+    });
 }
 
-function navContact(){
+function navContact() {
     const contactElem = document.querySelector(".nav-list__contact");
     contactElem.addEventListener("click", () => {
-       app.innerHTML = Contact();
-    });   
+        app.innerHTML = Contact();
+    });
 }
