0f89307133c8cdac142e38ce5aa964e3d6b47dc7
nichofer <nvasaris@kent.edu>
2022-02-10 16:50:44 -0500

    added contact styling

--- client-side/js/components/Contact.js
+++ client-side/js/components/Contact.js
@@ -13,7 +13,8 @@ export default function Contact(){
             <div class="contact-map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3055.6534975791105!2d-83.01442788435982!3d40.01618678782552!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88388ea08729618b%3A0x70ced8781fa94d9d!2sWe%20Can%20Code%20IT!5e0!3m2!1sen!2sus!4v1644095697631!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
-            <div class="feedback-form">
+        </div>
+        <div class="feedback-form">
             <h2>Send us your feedback</h2>
             <form>
                 <br>
@@ -29,8 +30,7 @@ export default function Contact(){
                         <textarea name="comments" rows="15" cols="50"></textarea><br><br>
                         
                     <input type="submit" value="Submit">
-                </form>
-            </div>
+            </form>
         </div>
     </section>
     
