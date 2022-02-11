eda02f923da7716ca4de9e96875af8773366dfca
nichofer <nvasaris@kent.edu>
2022-02-10 21:11:56 -0500

    reminders testing

--- client-side/js/components/AllReminders.js
+++ client-side/js/components/AllReminders.js
@@ -1,94 +1,26 @@
-export default function Reminders(reminders){
-    console.log('reminder page')
+export default function AllReminders(reminders) {
     return `
     <div class="api-page-container">
     <p>Reminder info</p>
     <ul>
     ${reminders.map(reminder => {
-        return `
-        <li class="">${reminder.name}
-            <input type='hidden' id="" value="${reminder.id}</li>`;
-    }).join('')}
+    return `
+    <li class="reminder">${reminder.name}
+        <input type='hidden' id="reminder-id" value="${reminder.id}"</li>`;
+    }).join('')};
     </ul> 
-    <h2 class="calendar">
-    March 2022
-</h2>
-<br />
-<table class="calendar">
-    <thead>
-        <tr>
-            <th class="calendar">
-                Sun</th>
-            <th class="calendar">
-                Mon</th>
-            <th class="calendar">
-                Tue</th>
-            <th class="calendar">
-                Wed</th>
-            <th class="calendar">
-                Thu</th>
-            <th class="calendar">
-                Fri</th>
-            <th class="calendar">
-                Sat</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr class="calendar">
-            <td></td>
-            <td></td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-        </tr>
-        <tr class="calendar">
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-        </tr>
-        <tr class="calendar">
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-        </tr>
-        <tr class="calendar">
-            <td>20</td>
-            <td>21</td>
-            <td>22</td>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-        </tr>
-        <tr class="calendar">
-            <td>27</td>
-            <td>28</td>
-            <td>29</td>
-            <td>30</td>
-            <td>31</td>
-            <td>1</td>
-            <td>2</td>
-        </tr>
-        <tr class="calendar">
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-        </tr>
-    </tbody>
-</table>
+    </div>
+    <div class="">
+    <div class="">
+            <form class="">
+                <label>Enter a Reminder</label>
+                <input type="text" class="add-reminder__name" placeholder="Enter Name">
+                <input type="text" class="add-reminder__category" placeholder="Enter Category">
+                <input type="text" class="add-reminder__priority" placeholder="Enter Priority">
+                <input type="text" class="add-reminder__description" placeholder="Enter Description">
+                <button type="submit" class="add-reminder__submit" onclick="AllReminders(reminders)">SUBMIT</button>
+            </form>
+    </div>
+    </div>  
     `;
 }
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -18,7 +18,7 @@ buildPage();
 function buildPage() {
     renderHome();
     navHome();
-    navReminders();
+    navAllReminders();
     navJournal();
     navAbout();
     navContact();
@@ -42,29 +42,88 @@ function checkin() {
     const checkin = document.querySelector(".checkin");
     checkin.addEventListener("click", () => {
         app.innerHTML = Mood();
-        var slider=document.getElementById("slider");
-        var emoji=document.getElementById("emoji");
-        var emoticons=["mood_bad",
-        "sentiment_very_dissatisfied",
-        "sentiment_satisfied",
-        "sentiment_satisfied_alt",
-        "sentiment_very_satisfied"
+        var slider = document.getElementById("slider");
+        var emoji = document.getElementById("emoji");
+        var emoticons = ["mood_bad",
+            "sentiment_very_dissatisfied",
+            "sentiment_satisfied",
+            "sentiment_satisfied_alt",
+            "sentiment_very_satisfied"
         ];
 
-        slider.oninput=function(){
-            emoji.innerHTML=emoticons[slider.value];
+        slider.oninput = function () {
+            emoji.innerHTML = emoticons[slider.value];
             console.log(slider.value);
         }
     });
 }
 
-function navReminders() {
+function navAllReminders() {
     const remindersElem = document.querySelector(".nav-list__reminders");
     remindersElem.addEventListener("click", () => {
-        crud.getRequest("http://localhost:8080/api/reminders", reminders => {
-            console.log(reminders);
+        apiHelpers.getRequest("http://localhost:8080/api/reminders", reminders => {
             app.innerHTML = AllReminders(reminders);
         });
+        renderReminders();
+    });
+}
+
+function renderReminders() {
+    app.addEventListener("click", (event) => {
+
+        if (event.target.classList.contains("reminder")) {
+            const id = event.target.querySelector("#reminder-id").value;
+            apiHelpers.getRequest(`http://localhost:8080/api/reminders/${id}`, (reminder) => {
+                app.innerHTML = Reminders(reminder);
+
+            });
+            returnToAllReminders();
+            addReminder();
+        }
+    });
+}
+
+function addReminder() {
+    app.addEventListener("click", (event) => {
+        if (event.target.classList.contains("add-reminder__submit")) {
+            const addResourceName = event.target.parentElement.querySelector(
+                ".add-reminder__name"
+            ).value;
+            const addResourceCategory = event.target.parentElement.querySelector(
+                ".add-reminder__category"
+            ).value;
+            const addResourcePriority = event.target.parentElement.querySelector(
+                ".add-reminder__priority"
+            ).value;
+            const addResourceDescription = event.target.parentElement.querySelector(
+                ".add-reminder__description"
+            ).value;
+
+            apiHelpers.postRequest(
+                "http://localhost:8080/api/reminders/add-reminder", {
+                    name: addResourceName,
+                    category: addResourceCategory,
+                    priority: addResourcePriority,
+                    description: addResourceDescription,
+                },
+                (reminders) =>
+                (app.innerHTML = AllReminders(reminders))
+            );
+        }
+    });
+}
+
+
+function returnToAllReminders() {
+    app.addEventListener("click", (event) => {
+        if (event.target.classList.contains("returnReminders")) {
+            apiHelpers.getRequest(
+                "http://localhost:8080/api/reminders",
+                (reminders) => {
+                    app.innerHTML = AllReminders(reminders);
+                }
+            );
+        }
     });
 }
 
