a91e070117967ae80ec38f163f579f21c4265782
DevDad1 <ryan@ryanwebster.dev>
2022-02-13 10:36:31 -0500

    added image to journal.js

--- client-side/js/components/Journal.js
+++ client-side/js/components/Journal.js
@@ -1,8 +1,11 @@
+import journal from "../../assets/journaling.jpg";
+
 export default function Journal(){
     return `
     <div class="container" id="app">
         <h1>Journal Entry</h1>
         <p>Description of journaling</p>
+        <img class="journal-entry-img" src="${journal}" alt="journal">
         <form>
             <label for="date">Date:</label><br>
             <input id="date" type="date">
