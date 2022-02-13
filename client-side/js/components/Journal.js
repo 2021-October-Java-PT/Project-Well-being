fd13b889b44edcfcaf7a916beda605048e8137a7
DevDad1 <ryan@ryanwebster.dev>
2022-02-13 10:26:20 -0500

    added form to journal.js

--- client-side/js/components/Journal.js
+++ client-side/js/components/Journal.js
@@ -1,7 +1,15 @@
 export default function Journal(){
     return `
     <div class="container" id="app">
-    <h1>Journal test</h1>
+        <h1>Journal Entry</h1>
+        <p>Description of journaling</p>
+        <form>
+            <label for="date">Date:</label><br>
+            <input id="date" type="date">
+            <label for="journal-entry">Journal Entry:</label><br>
+            <textarea id="journal-entry" name="content" rows="5" cols="66">Type your journal entry here...</textarea>
+            <input type="submit">
+        </form>
     </div>
     `
 }
