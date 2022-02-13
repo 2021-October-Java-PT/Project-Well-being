682527caa2d7b12acd7a837b33a79db657cb2170
DevDad1 <ryan@ryanwebster.dev>
2022-02-13 11:11:15 -0500

    styled Journal.js and made responsive

--- client-side/js/components/Journal.js
+++ client-side/js/components/Journal.js
@@ -3,14 +3,14 @@ import journal from "../../assets/journaling.jpg";
 export default function Journal(){
     return `
     <div class="container" id="app">
-        <h1>Journal Entry</h1>
-        <p>Description of journaling</p>
-        <img class="journal-entry-img" src="${journal}" alt="journal">
-        <form>
+        <h1 class="journal">Journal Entry</h1>
+        <p class="journal">Description of journaling</p>
+        <img  class="journal-entry-img" src="${journal}" alt="journal">
+        <form class="journal">
             <label for="date">Date:</label><br>
-            <input id="date" type="date">
+            <input id="date" type="date"><br><br>
             <label for="journal-entry">Journal Entry:</label><br>
-            <textarea id="journal-entry" name="content" rows="5" cols="66">Type your journal entry here...</textarea>
+            <textarea id="journal-entry" name="content" rows="5" cols="66">Type your journal entry here...</textarea><br>
             <input type="submit">
         </form>
     </div>
