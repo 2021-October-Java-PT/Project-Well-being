0607da59add190192d8f97713c4a97eed9be931f
Hesan Haghnazar <92179897+hhaghnazar@users.noreply.github.com>
2022-02-16 10:34:16 -0500

    Revert "Resources update2"

--- client-side/js/components/Mood.js
+++ client-side/js/components/Mood.js
@@ -1,11 +1,17 @@
-export default function Mood(){
-    return `
-      <div class="modal-body mood-content">
-        <h3>Feeling Scale</h3>
-        <i class="material-icons" id="emoji">sentiment_satisfied</i>
-        <input type="range" min="0" max="4" value="2" id="slider">
-        <br><br>
-        <button class="nextCheckin">Next</button>
-      </div>
+export default function Mood() {
+  return `
+    <div class="container" id="app">
+    <div class="wrapper">
+    <div class="mood-container" id="">
+        <div class="mood-content">
+            <i class="material-icons" id="emoji">
+            sentiment_satisfied
+            </i>
+            <input type="range" min="0" max="4" value="2" id="slider">
+            <button class="nextCheckin">Next</button>
+        </div>
+    </div>
+    </div>
+    </div>
     `;
 }
