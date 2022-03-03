dd6431bd8615dc593a651fdc4ead90ebe742b975
nichofer <nvasaris@kent.edu>
2022-03-03 15:46:04 -0500

    Updates

--- client-side/js/components/Journal.js
+++ client-side/js/components/Journal.js
@@ -4,7 +4,6 @@ import legalPad from "../../assets/legal-pad.png"
 export default function Journal(journals){
     return `
             <h1 class="journal">Journal Entry</h1>
-            <br>
             <p class="journal">Journaling about your feelings is linked to decreased mental distress. In a study, 
             researchers found that those with various medical conditions and anxiety who wrote online for 15 minutes 
             three days a week over a 12-week period had increased feelings of well-being and fewer depressive symptoms 
