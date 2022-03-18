69dfaa9e6d5a2556e4fc157ce9cccbc9e1549225
jordanCCIT <jordanadposting@gmail.com>
2022-03-17 21:07:51 -0400

    backend link swaps

--- src/main/java/org/wecancodeit/serverside/controllers/UserController.java
+++ src/main/java/org/wecancodeit/serverside/controllers/UserController.java
@@ -3,12 +3,14 @@ package org.wecancodeit.serverside.controllers;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wecancodeit.serverside.models.User;
 import org.wecancodeit.serverside.repos.UserRepository;
 import javax.annotation.Resource;
 import java.util.Optional;
 
+@RequestMapping("projectwellbeing-demo.herokuapp.com")
 @RestController
 @CrossOrigin
 public class UserController {
